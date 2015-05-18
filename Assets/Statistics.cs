6f389daf331d5995b65332f2fa4fb9c11972fefb
MocanuMarius <mariushtcwild@gmail.com>
2015-05-18 16:54:56 +0300

    Dfd

---
+++ Assets/Statistics.cs
@@ -0,0 +1,23 @@
+﻿
+//using MathNet.Numerics.Distributions;
+using Troschuetz.Random;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Statistics : MonoBehaviour {
+	// Use this for initialization
+	Distribution distrib;
+	public Text mytext;	
+	public Toggle myToggle;
+	void Start () {
+		distrib = new NormalDistribution ();
+		myToggle.Select ();
+	}
+	void Update () {
+		Debug.Log (distrib.Variance);
+		mytext.text = distrib.NextDouble ().ToString ();
+		mytext.text = Time.realtimeSinceStartup.ToString ();
+}
+}
+
